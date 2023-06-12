0ea00782b6647a41488e148e8ad2e81a2a936a64
evan-pieper <evanjpieper@gmail.com>
2023-06-12 16:03:02 -0700

    button now checks to see if it has already been clicked. if it has it removes the clickedTagButton class. If not it adds the class as normal.

--- tag button/tagButton.js
+++ tag button/tagButton.js
@@ -7,8 +7,13 @@ var tagButtons = document.querySelectorAll(".tagButton");
 tagButtons.forEach(function(currentBtn)  //changes the color of the tag button when clicked
 {
   currentBtn.addEventListener('click', function tagClick() {
-    this.classList.add("clickedTagButton");
-    console.log("tagClick2");
+    if(this.classList.contains("clickedTagButton")){ //if the button is already clicked, remove the clickedTagButton class}
+        this.classList.remove("clickedTagButton");}
+
+    else{ //if the button is not clicked, add the clickedTagButton class
+        this.classList.add("clickedTagButton");
+        console.log("tagClick2");
+    }
   })
 })
 
