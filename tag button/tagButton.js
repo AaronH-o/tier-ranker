34f080f1bd02e74815ee05ac8094bec95ae2ac23
evan-pieper <evanjpieper@gmail.com>
2023-06-12 16:00:02 -0700

    buttons now get a "clickedTagButton" class so they can be used for searches

--- tag button/tagButton.js
+++ tag button/tagButton.js
@@ -2,14 +2,13 @@
 var buttonCount = tagButton.length;
 //console.log("check");
 
-function tagClick () {
-    //var tag = target.value;
-    //buttonInput.style = "background-color: #ffffff;";
-    console.log("tagClick");
-}
 
 var tagButtons = document.querySelectorAll(".tagButton");
-tagButtons.forEach(function(currentBtn){
-  currentBtn.addEventListener('click', tagClick)
+tagButtons.forEach(function(currentBtn)  //changes the color of the tag button when clicked
+{
+  currentBtn.addEventListener('click', function tagClick() {
+    this.classList.add("clickedTagButton");
+    console.log("tagClick2");
+  })
 })
 
