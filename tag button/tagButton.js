01b46c4845acb417cc09d7b4efe5113c7f763878
evan-pieper <evanjpieper@gmail.com>
2023-06-12 19:40:49 -0700

    started foundation for saving to memory (creating a stringified array in local storage to be accessed and stored)

--- tag button/tagButton.js
+++ tag button/tagButton.js
@@ -1,13 +1,18 @@
-//var tagButton = document.querySelectorAll(".tagButton");
+localStorage.setItem("tagCount", 0); //sets the tagCount to 0 when the page is loaded
+localStorage.setItem("itemList", JSON.stringify([])); //sets the itemList to an empty array when the page is loaded
+localStorage.setItem("tagList", JSON.stringify([])); //sets the tagList to an empty array when the page is loaded
+
+
+
+
+
+
 var buttonCount = tagButton.length;
-//console.log("check");
-
-
 var tagButtons = document.querySelectorAll(".tagButton");
-tagButtons.forEach(function(currentBtn)  //changes the color of the tag button when clicked
+tagButtons.forEach(function(currentBtn)  //adds an event listener to each tagButton that adds a class to change the color of the tag button when clicked and mark it as tagged
 {
   currentBtn.addEventListener('click', function tagClick() {
-    if(this.classList.contains("clickedTagButton")){ //if the button is already clicked, remove the clickedTagButton class}
+    if(this.classList.contains("clickedTagButton")){ //if the button is already clicked, remove the clickedTagButton class
         this.classList.remove("clickedTagButton");}
 
     else{ //if the button is not clicked, add the clickedTagButton class
@@ -17,3 +22,17 @@ tagButtons.forEach(function(currentBtn)  //changes the color of the tag button w
   })
 })
 
+
+
+var listAddButton = document.querySelectorAll(".addListButton");
+if(listAddButton.length > 0)
+{  //if there are any addListButtons on the page, add an event listener to each one
+  listAddButton.forEach(function(currentBtn)
+  {  //adds an event listener to each addListButton that adds the item to the list when clicked
+    currentBtn.addEventListener('click', function listAddButton()
+    {
+      var list = document.querySelector(".listInput");
+
+    });
+  });
+}
